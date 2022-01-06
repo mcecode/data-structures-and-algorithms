e8f9116ae76c482b60e3f22c438f5f50790a10e6
Matthew Espino <65783406+mcecode@users.noreply.github.com>
2022-01-06 17:55:55 +0800

    PHP: Replace null in union types with ?

--- php/Interfaces/LinkedList.php
+++ php/Interfaces/LinkedList.php
@@ -6,54 +6,54 @@ namespace Interfaces;
 
 interface LinkedList
 {
-  public function getHead(): LinkedListNode|null;
-  public function getTail(): LinkedListNode|null;
+  public function getHead(): ?LinkedListNode;
+  public function getTail(): ?LinkedListNode;
 
   public function insertBeforeHead(mixed $value): LinkedListNode;
   public function insertBeforeTail(mixed $value): LinkedListNode;
   public function insertBefore(
     mixed $searchValue,
     mixed $value
-  ): LinkedListNode|null;
+  ): ?LinkedListNode;
   public function insertBeforeAt(
     int $position,
     mixed $value
-  ): LinkedListNode|null;
+  ): ?LinkedListNode;
 
   public function insertAfterHead(mixed $value): LinkedListNode;
   public function insertAfterTail(mixed $value): LinkedListNode;
   public function insertAfter(
     mixed $searchValue,
     mixed $value
-  ): LinkedListNode|null;
+  ): ?LinkedListNode;
   public function insertAfterAt(
     int $position,
     mixed $value
-  ): LinkedListNode|null;
+  ): ?LinkedListNode;
 
   public function insertBetween(
     mixed $beforeValue,
     mixed $afterValue,
     mixed $value
-  ): LinkedListNode|null;
+  ): ?LinkedListNode;
 
-  public function find(mixed $value): LinkedListNode|null;
-  public function findAt(int $position): LinkedListNode|null;
+  public function find(mixed $value): ?LinkedListNode;
+  public function findAt(int $position): ?LinkedListNode;
 
   public function replace(
     mixed $oldValue,
     mixed $newValue
-  ): LinkedListNode|null;
+  ): ?LinkedListNode;
   public function replaceAt(
     int $position,
     mixed $newValue
-  ): LinkedListNode|null;
+  ): ?LinkedListNode;
 
-  public function delete(mixed $value): LinkedListNode|null;
-  public function deleteAt(int $position): LinkedListNode|null;
-  public function deleteHead(): LinkedListNode|null;
-  public function deleteTail(): LinkedListNode|null;
+  public function delete(mixed $value): ?LinkedListNode;
+  public function deleteAt(int $position): ?LinkedListNode;
+  public function deleteHead(): ?LinkedListNode;
+  public function deleteTail(): ?LinkedListNode;
 
   public function count(): int;
-  public function reverse(): LinkedListNode|null;
+  public function reverse(): ?LinkedListNode;
 }
