a26ca6b7c6196e602d4ba0863d18522b66c00574
Matthew Espino <65783406+mcecode@users.noreply.github.com>
2022-01-27 18:01:30 +0800

    PHP: Simplify LinkedList interface

--- php/Interfaces/LinkedList.php
+++ php/Interfaces/LinkedList.php
@@ -9,22 +9,20 @@ interface LinkedList
   public function getHead(): ?LinkedListNode;
   public function getTail(): ?LinkedListNode;
 
+  public function insertHead(mixed $value): LinkedListNode;
+  public function insertTail(mixed $value): LinkedListNode;
   public function insertBefore(
     mixed $searchValue,
     mixed $value
   ): ?LinkedListNode;
-  public function insertBeforeHead(mixed $value): LinkedListNode;
   public function insertBeforeTail(mixed $value): LinkedListNode;
   public function insertBeforeAt(int $position, mixed $value): ?LinkedListNode;
-
   public function insertAfter(
     mixed $searchValue,
     mixed $value
   ): ?LinkedListNode;
   public function insertAfterHead(mixed $value): LinkedListNode;
-  public function insertAfterTail(mixed $value): LinkedListNode;
   public function insertAfterAt(int $position, mixed $value): ?LinkedListNode;
-
   public function insertBetween(
     mixed $beforeValue,
     mixed $afterValue,
@@ -37,10 +35,10 @@ interface LinkedList
   public function replace(mixed $oldValue, mixed $newValue): ?LinkedListNode;
   public function replaceAt(int $position, mixed $newValue): ?LinkedListNode;
 
-  public function delete(mixed $value): ?LinkedListNode;
-  public function deleteAt(int $position): ?LinkedListNode;
   public function deleteHead(): ?LinkedListNode;
   public function deleteTail(): ?LinkedListNode;
+  public function delete(mixed $value): ?LinkedListNode;
+  public function deleteAt(int $position): ?LinkedListNode;
 
   public function count(): int;
   public function reverse(): ?LinkedListNode;
