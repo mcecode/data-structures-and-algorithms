a5aee6e81ed309fe10cc8e70aef01a25c4654ad3
Matthew Espino <65783406+mcecode@users.noreply.github.com>
2022-10-27 00:08:55 +0800

    Initialize TypeScript with two sum

---
+++ typescript/1-two-sum.ts
@@ -0,0 +1,17 @@
+// O(n^2)
+function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+
+  return [];
+}
+
+// O(n)
+function twoSumV2(nums: number[], target: number): number[] {
+  return [];
+}
